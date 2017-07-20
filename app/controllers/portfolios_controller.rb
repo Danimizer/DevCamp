c817d844ac15c6eb5bd7b90b36a1f4f471a39bc1
Danimizer <jordan@devcamp.com>
2017-07-20 17:16:53 +0000

    updated portfolio to include attr for postition

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,27 +1,27 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
-  
+
   def angular
-     @angular_portfolio_items = Portfolio.angular
+    @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
-  end 
-  
+  end
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -29,11 +29,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    
   end
-  
+
   def update
-    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -42,16 +40,14 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
   end
-  
+
   def destroy
-    # Perform the lookup
-    
-    #Destroy/delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
@@ -61,17 +57,14 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-  params.require(:portfolio).permit(:title, 
-                                    :subtitle, 
-                                    :body, 
-                                    technologies_attributes: [:name]
-                                    )
-
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
   end
 
   def set_portfolio_item
-     @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
-
-
